39db1d72b87bcdddacdab1dd0d2557bdfa1c8ab1
Vladimir <Vladimir@DESKTOP-3QT542Q>
2023-11-22 18:32:31 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte simpleCode;
+            sbyte webMask;
+            short number;
+            ushort orderNumber;
+            int countHorse;
+            uint distance;
+            long solarSystemMass;
+            ulong lenghtManhood;
+            float healthPoints;
+            double exactWeightObject;
+            decimal electronRadius;
+            char symbol;
+            string userInput;
+            bool isAvailable;
+            object somethingWrong;
+            var greetings = "Hello World!";
+        }
+    }
+}
